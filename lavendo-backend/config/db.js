edef2a7e812025b20f50dfbf437e655d317f8804
Niyatizzz <niyatikolukula@gmail.com>
2025-05-30 22:53:50 +0530

    db setup

--- lavendo-backend/config/db.js
+++ lavendo-backend/config/db.js
@@ -1,5 +1,9 @@
 import mongoose, { connect } from "mongoose";
 
 export const connectDB = async() => {
-    await mongoose.connect('')
+    await mongoose.connect(
+      "mongodb+srv://niyatikolukula:Qwerty%402516@cluster0.tfkkqqa.mongodb.net/lavendo"
+    ) .then(() => {
+        console.log('DB connected')
+    })
 }
--- lavendo-backend/sever.js
+++ lavendo-backend/sever.js
@@ -1,11 +1,22 @@
 import express from 'express'
+import cors from 'cors'
+import { connectDB } from './config/db.js';
 
 const app = express();
 const port = process.env.PORT || 4000;
 
 //middleware
-
 app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({extended: true}))
+
+//db connect
+connectDB();
+
+//routes
+app.get('/', (req, res) => {
+    res.send('API working')
+})
 
 app.listen(port, () => {
     console.log(`server running on ${port}`)
