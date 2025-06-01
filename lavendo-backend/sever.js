965523fed2a32ac50e1fccfc96fbf5002470704e
Niyatizzz <niyatikolukula@gmail.com>
2025-06-01 12:27:28 +0530

    task router

--- lavendo-backend/sever.js
+++ lavendo-backend/sever.js
@@ -4,6 +4,7 @@ import { connectDB } from './config/db.js';
 import dotenv from 'dotenv'
 
 import userRouter from './routes/userRoute.js'
+import taskRouter from './routes/taskRoute.js';
 
 dotenv.config()
 
@@ -20,6 +21,7 @@ connectDB();
 
 //routes
 app.use('/api/user', userRouter)
+app.use("/api/tasks", taskRouter);
 
 app.get('/', (req, res) => {
     res.send('API working')
